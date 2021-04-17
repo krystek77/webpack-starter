29f055f20f1053ad51881665938a3e0402b1b217
krystek77 <krystian.wrona77@gmail.com>
2021-04-17 17:46:37 +0200

    Install plugin CopyWebpack to copy asstets to dist

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
   output: {
@@ -13,6 +14,14 @@ module.exports = {
       minify: 'auto',
     }),
     new MiniCSSExtractPlugin({ filename: 'index.css' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/assets'),
+          to: path.resolve(__dirname, '../dist/assets'),
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
