e5c60b09e3c111ebfe211bfdad8987b103d1498e
krystek77 <krystian.wrona77@gmail.com>
2021-04-17 18:33:22 +0200

    Remove style-loader and copyWebpackplugin

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
   output: {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './css/index.css';
-console.log('Boilerplate webpack application ');
+console.log('Boilerplate webpack application... ');
 import pic from './assets/images/0.png';
 console.log(pic);
